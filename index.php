efe157701f8c12bc1e3393ed2c3780d5a656da4b
JouieBoks <50953068+jbLearns@users.noreply.github.com>
2019-06-26 23:54:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-echo "test";
-?><head>
+<!DOCTYPE html>
+<html>
+<head>
 	<title>PHP - OOP CRUD Operation using AJAX/jQuery</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
